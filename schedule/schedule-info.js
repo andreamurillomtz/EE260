055bc05e74a9dca7f5c55a6a0b2f91ea73c807ae
andreamurillomtz <andreamurillomtz@users.noreply.github.com>
2023-10-25 03:23:26 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,22 @@
+moreaEventData = [
+  {title: "Binary Decoder", url: "/EETest/modules/example-introduction", start: "2021-07-06", end: "2021-07-09T23:00"},
+  {title: "E01: Environment configuration", url: "/EETest/morea/example-introduction/experience-environment-configuration.html", start: "2021-07-07T23:00"},
+  {title: "E03: Learn the command line", url: "/EETest/morea/example-introduction/experience-learn-the-command-line.html", start: "2021-07-09T23:00"},
+  {title: "E02: Introduce yourself", url: "/EETest/morea/example-introduction/experience-introduce-yourself.html", start: "2021-07-08T23:00"},
+  {title: "Logic Gates", url: "/EETest/modules/logic-gates", start: "2021-07-26", end: "2021-07-30T23:00"},
+  {title: "A03: Assess your ability to reason about ethical issues in software development", url: "/EETest/morea/logic-gates/assessment-logic-test.html", start: "2021-07-30T09:00"},
+  {title: "E09: Doing good as a software engineer", url: "/EETest/morea/logic-gates/experience-logic-test.html", start: "2021-07-27T23:00"},
+  {title: "A03: Assess your ability to reason about ethical issues in software development", url: "/EETest/morea/example-verilog/assessment-verilog.html", start: "2021-07-30T09:00"},
+  {title: "E09: Doing good as a software engineer", url: "/EETest/morea/example-verilog/experience-se-ethics-good.html", start: "2021-07-27T23:00"},
+  {title: "E10: Doing harm as a software engineer", url: "/EETest/morea/example-verilog/experience-se-ethics-harm.html", start: "2021-07-28T23:00"},
+  {title: "Verilog", url: "/EETest/modules/verilog", start: "2021-07-26", end: "2021-07-30T23:00"},
+  {title: "E08: What are the professional codes of software engineering ethics?", url: "/EETest/morea/example-verilog/experience-se-ethics-codes.html", start: "2021-07-26T23:00"},
+  {title: "E07: Reflect on smart questions", url: "/EETest/morea/example-open-source-software/experience-smart-questions.html", start: "2021-07-22T23:00"},
+  {title: "Open Source Software", url: "/EETest/modules/example-open-source-software", start: "2021-07-19", end: "2021-07-23T23:00"},
+  {title: "A02: Assess your ability to write effectively about open source software", url: "/EETest/morea/example-open-source-software/assessment-open-source-software.html", start: "2021-07-23T09:00"},
+  {title: "E04: Introduction to Javascript with FreeCodeCamp", url: "/EETest/morea/example-javascript/experience-interactive-javascript-1.html", start: "2021-07-13T23:00"},
+  {title: "E06: Reflect on Javascript 1", url: "/EETest/morea/example-javascript/experience-javascript-1-technical-essay.html", start: "2021-07-15T23:00"},
+  {title: "E05: isUnique", url: "/EETest/morea/example-javascript/experience-ctci.html", start: "2021-07-14T23:00"},
+  {title: "A01: Assessment of simple Javascript skills", url: "/EETest/morea/example-javascript/assessment-javascript-1.html", start: "2021-07-16T09:00"},
+  {title: "Flip Flop", url: "/EETest/modules/example-javascript", start: "2021-07-12", end: "2021-07-16T23:00"}
+]
